298ffd4d2fe0c98756b4ae67e2f868a6e40badbf
Vladimir-Mokanj <79630981+Vladimir-Mokanj@users.noreply.github.com>
2023-10-20 12:11:49 +0200

    Basic input controller
    Added a way to communicate and fire events from data that is gathered during presses

---
+++ Assets/_Production/Scripts/FireTale/Inputs/InputData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace FT.Inputs
+{
+    public class InputData
+    {
+        public Vector2 moveDirection;
+        public bool isShooting;
+    }
+}
--- Assets/_Production/Scripts/FireTale/Inputs/PlayerInputController.cs
+++ Assets/_Production/Scripts/FireTale/Inputs/PlayerInputController.cs
@@ -1,4 +1,7 @@
+using System;
+using FT.Tools.Observers;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace FT.Inputs
 {
@@ -6,6 +9,15 @@ namespace FT.Inputs
     [DisallowMultipleComponent]
     public class PlayerInputController : MonoBehaviour, IInputProvider
     {
+        private readonly InputData _inputData = new();
+        
+        public IObservableAction<Action<InputData>> OnInput => _onInput;
+        private readonly ObservableAction<Action<InputData>> _onInput = new();
 
+        public void OnMove(InputValue value) => _inputData.moveDirection = value.Get<Vector2>();
+        public void OnShoot(InputValue value) => _inputData.isShooting = value.isPressed;
+        
+        private void Update() => 
+            _onInput.Action?.Invoke(_inputData);
     }
 }
