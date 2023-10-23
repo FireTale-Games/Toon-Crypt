1b4e966339b09fbf6a464699b01fd82bb5fe2362
Vladimir-Mokanj <79630981+Vladimir-Mokanj@users.noreply.github.com>
2023-10-23 19:52:59 +0200

    Bullet particle

--- Assets/_Production/Scripts/FireTale/Ability/States/ExplosionState.cs
+++ Assets/_Production/Scripts/FireTale/Ability/States/ExplosionState.cs
@@ -7,10 +7,24 @@ namespace FT.Ability.States
 {
     public class ExplosionState : AbilityState
     {
+        [SerializeField] private Transform _explosionEffect;
+        private float _delayBeforeDestroy;
+        
+        private void Awake()
+        {
+            foreach (ParticleSystem ps in _explosionEffect.GetComponentsInChildren<ParticleSystem>())
+            {
+                if (ps.main.startLifetime.constantMax < _delayBeforeDestroy)
+                    continue;
+
+                _delayBeforeDestroy = ps.main.startLifetime.constantMax;
+            }
+        }
+
         public override void Execute(List<Data.Ability> abilities)
         {
             StatsController.ApplyDamage(AbilityInfo.Damage);
-            Collider[] hits = Physics.OverlapSphere(transform.position, 5.0f);
+            Collider[] hits = Physics.OverlapSphere(transform.position, 2.0f);
             foreach (Collider col in hits)
             {
                 if (col.transform == StatsController.transform || col.transform.TryGetComponent(out IHit hit) == false)
@@ -21,7 +35,7 @@ namespace FT.Ability.States
             }
             
             _onDispose.Action?.Invoke(this);
-            Destroy(gameObject);
+            Destroy(gameObject, _delayBeforeDestroy);
         }
     }
 }
