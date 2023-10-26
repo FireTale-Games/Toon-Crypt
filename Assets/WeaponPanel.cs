6c2168265855ebdd6c629db342acda5f59c9946d
Vladimir-Mokanj <79630981+Vladimir-Mokanj@users.noreply.github.com>
2023-10-26 21:11:52 +0200

    Update 4
    Slots depend on the rarity of the weapon itself

--- Assets/WeaponPanel.cs
+++ Assets/WeaponPanel.cs
@@ -18,10 +18,10 @@ namespace FT.Inventory
             if (item == null && _weaponSlot.Id == -1)
                 RemoveAbilities();
             
-            if (item is not Weapon)
+            if (item is not Weapon weapon)
                 return;
             
-            AddAbilities();
+            AddAbilities(GetSlotNumber(weapon.Rarity));
         }
 
         private void RemoveAbilities()
@@ -32,16 +32,29 @@ namespace FT.Inventory
             _weaponRectTransform.sizeDelta = new Vector2(74 + (_weaponRectTransform.childCount - 1) * 67, _weaponRectTransform.sizeDelta.y);
         }
         
-        private void AddAbilities()
+        private void AddAbilities(int slotNumber)
         {
             RemoveAbilities();
             for (int i = _weaponRectTransform.childCount - 1; i >= 2; i--)
                 Destroy(_weaponRectTransform.GetChild(i).gameObject);
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < slotNumber; i++)
                 Instantiate(_abilityUi, _weaponRectTransform);
 
             _weaponRectTransform.sizeDelta = new Vector2(74 + (_weaponRectTransform.childCount - 1) * 67, _weaponRectTransform.sizeDelta.y);
+        }
+
+        private int GetSlotNumber(Rarity weaponRarity)
+        {
+            return weaponRarity switch
+            {
+                Rarity.Common => 1,
+                Rarity.Uncommon => 2,
+                Rarity.Rare => 3,
+                Rarity.Epic => 4,
+                Rarity.Legendary => 5,
+                _ => 0
+            };
         }
     }
 }
