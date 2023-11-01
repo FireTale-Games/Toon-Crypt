f17316ae29f9ac8268f2f080e08d06ceaf2793ad
Vladimir-Mokanj <79630981+Vladimir-Mokanj@users.noreply.github.com>
2023-11-01 11:35:33 +0100

    Character Event

--- Assets/_Production/Scripts/FireTale/TD/CharacterShooterController.cs
+++ Assets/_Production/Scripts/FireTale/TD/CharacterShooterController.cs
@@ -42,25 +42,27 @@ namespace FT.Shooting
 
         private IEnumerator StartShooting()
         {
-            if (_nextShootTime <= Time.time)
-            {
-                ShootProjectile().AddObserver(OnHit);
-                
-                _nextShootTime = Time.time + _shootDelay;
-                yield return new WaitForSeconds(_shootDelay);
-            }
+            yield break;
             
-            while (true)
-            {
-                if (_nextShootTime <= Time.time)
-                {
-                    ShootProjectile().AddObserver(OnHit);
-                    
-                    _nextShootTime = Time.time + _shootDelay;
-                }
+            //if (_nextShootTime <= Time.time)
+            //{
+            //    ShootProjectile().AddObserver(OnHit);
+            //    
+            //    _nextShootTime = Time.time + _shootDelay;
+            //    yield return new WaitForSeconds(_shootDelay);
+            //}
+            //
+            //while (true)
+            //{
+            //    if (_nextShootTime <= Time.time)
+            //    {
+            //        ShootProjectile().AddObserver(OnHit);
+            //        
+            //        _nextShootTime = Time.time + _shootDelay;
+            //    }
 
-                yield return null;
-            }
+            //    yield return null;
+            //}
         }
         
         private IObservableAction<Action<IHit>> ShootProjectile()
